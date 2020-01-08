b5c9ec4d4a6d66196410c6e51ec88366e4df90bf
Christopher Millet <chris@Christophers-iMac-2.local>
2020-01-08 13:59:22 -0500

    adding built files to repo[D

---
+++ build/precache-manifest.3888fb555d77918289b74600e6578a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db207d986d6acc334a0f28c5d3b39b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e41cc7dc8a18a62ae69a",
+    "url": "/static/css/main.4f48b4ca.chunk.css"
+  },
+  {
+    "revision": "5e92771bef753cda785e",
+    "url": "/static/js/2.a17d9f31.chunk.js"
+  },
+  {
+    "revision": "e41cc7dc8a18a62ae69a",
+    "url": "/static/js/main.1cbc700a.chunk.js"
+  },
+  {
+    "revision": "c457873315028294a926",
+    "url": "/static/js/runtime~main.33915ab7.js"
+  }
+]);
